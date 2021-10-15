c4d02ba102a2d537f02990cf7e3a40b9dbadd028
Pablo Garrido <pablogs9@gmail.com>
2021-10-15 13:26:11 +0200

    Add embeddedRTPS as experimental middleware (#29)
    * Initial
    
    * Update
    
    * Update
    
    * Update
    
    * Update
    
    * Fix image
    
    * Fix link
    
    * Update
    
    * Update
    
    * Update
    
    * Update
    
    * Add rtps conf to .meta
    
    * Apply suggestions from code review
    
    Co-authored-by: Antonio Cuadros <49162117+Acuadros95@users.noreply.github.com>
    
    Co-authored-by: Antonio Cuadros <acuadros1995@gmail.com>
    Co-authored-by: Antonio Cuadros <49162117+Acuadros95@users.noreply.github.com>

--- microros_static_library_ide/library_generation/extract_flags.py
+++ microros_static_library_ide/library_generation/extract_flags.py
@@ -7,14 +7,35 @@ mfpu = [x for x in text if x.startswith("-mfpu")]
 mfloatabi = [x for x in text if x.startswith("-mfloat-abi")]
 mthumb = [x for x in text if x.startswith("-mthumb")]
 optimization = [x for x in text if x.startswith("-O")]
+specs = [x for x in text if x.startswith("--specs")]
 
-out = "-ffunction-sections -fdata-sections"
-if len(mcpu) and len(mfpu) and len(mfloatabi):
-    out = out + " " + mcpu[0] + " " + mfpu[0] + " " + mfloatabi[0]
+defines = [x for x in text if x.startswith("-D")]
+defines = list(set(defines))
+
+includes = [x for x in text if x.startswith("-I")]
+includes = list(set(includes))
+includes = [x.replace("../", "/project/") for x in includes]
+
+out = "-ffunction-sections -fdata-sections -DSTM32CUBEIDE"
+
+# This fix is needed for the embeddedRTPS to work due to the lack of include
+# <sys/errno.h> in Middlewares/Third_Party/LwIP/src/include/lwip/errno.h
+out = out + " -DENOTSUP=1 -DECANCELED=1 -DEOWNERDEAD=1 -DENOTRECOVERABLE=1"
+
+if len(mcpu) and len(mfloatabi):
+    out = out + " " + mcpu[0] + " " + mfloatabi[0]
     if len(mthumb):
         out = out + " " + mthumb[0]
+    if len(mfpu):
+        out = out + " " + mfpu[0]
     if len(optimization):
         out = out + " " + optimization[0]
+    if len(defines):
+        out = out + " " + " ".join(defines)
+    if len(specs):
+        out = out + " " + specs[0]
+    if len(includes):
+        out = out + " " + " ".join(includes)
     print(out)
     sys.exit(0)
 else:
